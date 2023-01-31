8d122f53963f036e7dfe60188b190ba675166214
Shruti Yadav <58552062+shruti01052002@users.noreply.github.com>
2023-01-31 20:27:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <>
             <div className="foot_style">
-                <h1 className="made_with">made by build buddy team ❤️</h1>
+                <h1 className="made_with">Made with ❤️</h1>
             </div>
         </>
     );
